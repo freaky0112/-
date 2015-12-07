dfbebd60388e4bc41f1c4f54d4e3308dee505a79
freaky0112 <freakoutelf@gmail.com>
2015-12-07 13:40:47 +0800

    move mono to vs

---
+++ DLPD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace DLPD
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
